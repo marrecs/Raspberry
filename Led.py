ef861df5c54bb26a2e317412f136d093434a6de8
marrecs <marrecsdevilassar@gmail.com>
2018-11-16 22:54:49 +0000

    Poso comentaris

--- Led.py
+++ Led.py
@@ -1,13 +1,26 @@
+#16-11-2018
+#Encendre un Led
+
+#Importem llibreries
 import RPi.GPIO as GPIO
 import time
+
 
 GPIO.setmode(GPIO.BCM)
 #GPIO.setwarnings(False)
 
+#Definim numero de Pin
 GPIO.setup(6,GPIO.OUT)
+
+
+#Encenem Led
 print "LED on"
 GPIO.output(6,GPIO.HIGH)
+
+#Esperem
 time.sleep(10)
+
+#Apaguem Led
 print "LED off"
 GPIO.output(6,GPIO.LOW)
 
